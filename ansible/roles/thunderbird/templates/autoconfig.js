9e195b5f8abda18206d15317ac32ef5af168ca8c
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2024-06-20 21:36:23 +0200

    A bunch of modifications (doesn't totally work as it seems like codium requires umake - oh the atrocity)

---
+++ ansible/roles/thunderbird/templates/autoconfig.js
@@ -0,0 +1,7 @@
+// See https://enterprise.thunderbird.net/deploy/mcd-thunderbird-autoconfig#setting-up-autoconfiguration
+// for details
+
+// Set this to 0 to make sure I can use cleartext file
+pref("general.config.obscure_value", 0);
+// Location of the filename that contains all config
+pref("general.config.filename", "thunderbird.cfg");
