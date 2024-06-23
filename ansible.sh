e1049230403fab9cbfcf7c556762d78052fa58d6
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2024-06-23 19:03:10 +0200

    And now it runs!

--- ansible.sh
+++ ansible.sh
@@ -17,11 +17,11 @@ zorinUser={zorinUser:-$defaultKeepass}
 # Now read values from keepassxc database
 # keepassxc command-line is obviously slightly different
 
-zorinPassword=$(echo $password | keepassxc-cli show /home/nicolas/kDrive/personnel.kdbx "Infomaniak" --show-protected --quiet --attributes password)
+zorinPassword=$(echo $password | keepassxc-cli show /home/nicolas/kDrive/personnel.kdbx "Portable Dell" --show-protected --quiet --attributes password)
 kdrivePassword=$(echo $password | keepassxc-cli show /home/nicolas/kDrive/personnel.kdbx "Infomaniak" --show-protected --quiet --attributes password)
 
 currentFolder=${PWD}
 # Finally start the docker image!
 docker="docker run --rm --name ansible -t -i -e ZORIN_PASSWORD=\"$zorinPassword\" -e KDRIVE_PASSWORD=\"$kdrivePassword\" -v $currentFolder/ansible:/ansible willhallonline/ansible:2.13-ubuntu-22.04 /bin/bash"
 
-sudo $docker
+sudo bash -c "$docker"
