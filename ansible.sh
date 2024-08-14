28df019f1d418e35e5ae22ca2b0e499ae7cc814d
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2024-08-14 18:50:26 +0200

    Upgraded ansible script to have it finally running

--- ansible.sh
+++ ansible.sh
@@ -2,27 +2,27 @@
 
 defaultKeepass=$HOME/kDrive/personnel.kdbx
 read -p  "Quelle base Keepass utiliser? [$defaultKeepass]" keepass
-keepass={keepass:-$defaultKeepass}
+keepass="${keepass:-$defaultKeepass}"
 
 stty -echo
-read -p "Keepass master password: " password
+read -p "Quel est le mot de passe de '$keepass'?: " password
 stty echo
 printf '\n'
 
 
 defaultZorinUser="nicolas-delsaux"
 read -p  "Quel est l'utilisateur Linux? [$defaultZorinUser]" zorinUser
-zorinUser={zorinUser:-$defaultKeepass}
+zorinUser="${zorinUser:-$defaultKeepass}"
 
 # Now read values from keepassxc database
 # keepassxc command-line is obviously slightly different
 
-sessionPassword=$(echo $password | keepassxc-cli show $keepass "Portable Dell" --show-protected --quiet --attributes password)
-kdrivePassword=$(echo $password | keepassxc-cli show $keepass "Infomaniak" --show-protected --quiet --attributes password)
+sessionPassword=$(echo "$password" | keepassxc-cli show $keepass "Portable Dell" --show-protected --quiet --attributes "password")
+kdrivePassword=$(echo "$password" | keepassxc-cli show $keepass "Infomaniak" --show-protected --quiet --attributes "password")
 
 currentFolder=${PWD}
 # Finally start the docker image!
 # See https://stackoverflow.com/a/36648428 for the ssh socket madness
 docker="docker run --rm --name ansible -t -i --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent -e SESSION_PASSWORD=\"$sessionPassword\" -e KDRIVE_PASSWORD=\"$kdrivePassword\" -v $currentFolder/ansible:/ansible:ro willhallonline/ansible:2.16.4-bookworm-slim /bin/bash"
 
-sudo bash -c "$docker"
+bash -c "$docker"
