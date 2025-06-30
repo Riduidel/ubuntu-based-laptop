5417899dce8d8a2b577ffaa59d7efbea262f4a17
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2025-06-30 12:44:02 +0200

    Things weren't correctly named

--- ansible.sh
+++ ansible.sh
@@ -10,9 +10,9 @@ stty echo
 printf '\n'
 
 
-defaultZorinUser="nicolas-delsaux"
-read -p  "Quel est l'utilisateur Linux? [$defaultZorinUser]" zorinUser
-zorinUser="${zorinUser:-$defaultKeepass}"
+DEFAULT_SYSTEM_USER="nicolas-delsaux"
+read -p  "Quel est l'utilisateur Linux? [$DEFAULT_SYSTEM_USER]" systemUser
+systemUser="${systemUser:-$defaultKeepass}"
 
 # Now read values from keepassxc database
 # keepassxc command-line is obviously slightly different
@@ -23,6 +23,6 @@ kdrivePassword=$(echo "$password" | keepassxc-cli show $keepass "Infomaniak" --s
 currentFolder=${PWD}
 # Finally start the docker image!
 # See https://stackoverflow.com/a/36648428 for the ssh socket madness
-docker="docker run --rm --name ansible -t -i --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent -e SESSION_PASSWORD=\"$sessionPassword\" -e KDRIVE_PASSWORD=\"$kdrivePassword\" -v $currentFolder/ansible:/ansible:ro willhallonline/ansible:2.16.4-bookworm-slim /bin/bash"
+docker="sudo docker run --rm --name ansible -t -i --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent -e SESSION_PASSWORD=\"$sessionPassword\" -e KDRIVE_PASSWORD=\"$kdrivePassword\" -v $currentFolder/ansible:/ansible:ro willhallonline/ansible:2.16.4-bookworm-slim /bin/bash"
 
 bash -c "$docker"
